4d6d5406479cba3fe966c5345ada591d57799b32
Sepideh <108736695+sepidehtajdini@users.noreply.github.com>
2022-12-05 01:01:58 -0800

    Add files via upload

---
+++ App.js
@@ -0,0 +1,14 @@
+import Todo from "./6/Todo/Todo.js";
+import Sketch from "./6/Sketch//Sketch.js";
+
+function App() {
+
+  return (
+    <div>
+      <Todo />
+      <Sketch />
+    </div>
+  );
+}
+
+export default App;
