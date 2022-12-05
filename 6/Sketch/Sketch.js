916a5eb31427ac686ef25880e4c1e91a62fa4828
Sepideh <108736695+sepidehtajdini@users.noreply.github.com>
2022-12-05 11:53:28 +0330

    Create Sketch.js

---
+++ 6/Sketch/Sketch.js
@@ -0,0 +1,12 @@
+import Header from "./Components/Header/Header.js";
+import Content from "./Components/Content/Content.js"
+
+function Sketch() {
+    return (
+        <div>
+            <Header />
+            <Content />
+        </div>
+    )
+}
+export default Sketch
